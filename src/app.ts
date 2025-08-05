082de8dd6ca430f9f1cb73ddd502a37eacdc2dc6
Vitor Tognetti Baradelli <vitorbaradelli@gmail.com>
2025-08-05 14:37:13 -0300

    feat: add /hello-worlds endpoint with debug logging

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import cors from "cors";
 import express from "express";
 import { createServer } from "http";
+import { log } from "./infra/logger";
 
 const app = express();
 const server = createServer(app);
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/hello-worlds", (req, res) => {
+  log.debug("This is a debug message");
+
+  res.send("Hello, World!");
+});
 
 export default server;
